24b107b98e4ad24f8984428e25500a0c9723a7a2
Javier <javierrp94@gmail.com>
2024-02-06 10:53:19 +0100

    Create Ejercicio_02.js
    Creado y subido el Ejercicio 2 de la plataforma develhope sobre JavaScript

---
+++ Ejercicios/Curso_Oficial/JavaScript/Ejercicios/Ejercicio_02.js
@@ -0,0 +1,31 @@
+function nicknameMap(persons) {
+    const nicknames = [];
+  
+    for (let i = 0; i < persons.length; i++) {
+      const person = persons[i];
+      const nickname = `${person.name}-${person.age}`;
+      nicknames.push(nickname);
+    }
+  
+    return nicknames;
+  }
+  
+  const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+  ];
+  
+  const nicknames = nicknameMap(persons);
+  
+  
+  console.log(JSON.stringify(persons, null, 2));
+  console.log(nicknames);
+  
