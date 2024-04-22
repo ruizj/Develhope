6043a924c6eb2e95176480ef3803682e24e1a551
Javier <javierrp94@gmail.com>
2024-04-22 11:27:15 +0200

    Update Ejercicio_03.sql

---
+++ Ejercicios/Curso_Oficial/Sql-Module/Ejercicio_03.sql
@@ -0,0 +1,15 @@
+-- Insertar detalles del libro "Un viaje al centro" de Julio Verne
+INSERT INTO Libros (titulo, autor, genero, año_publicado, isbn, precio, calificacion, stock_count, editor, num_paginas)
+VALUES ('Un viaje al centro', 'Julio Verne', 'Aventura', 1864, '1234567890123', 12.99, 4.5, 10, 'Editorial Verne', 350);
+
+-- Insertar detalles del libro "Guerra y paz" de León Tolstoi
+INSERT INTO Libros (titulo, autor, genero, año_publicado, isbn, precio, calificacion, stock_count, editor, num_paginas)
+VALUES ('Guerra y paz', 'León Tolstoi', 'Histórico', 1869, '1234567890124', 14.99, 4.8, 5, 'Impresiones de Tolstoi', 1200);
+
+-- Insertar detalles del libro "Susurros del viento" de Amelia Blackburn
+INSERT INTO Libros (titulo, autor, genero, año_publicado, isbn, precio, calificacion, stock_count, editor, num_paginas)
+VALUES ('Susurros del viento', 'Amelia Blackburn', 'Romance', 1982, '1234567890125', 9.99, 4.2, 20, 'Casa Blackburn', 275);
+
+-- Insertar detalles del libro "La Odisea Galáctica" de Orion Starfield
+INSERT INTO Libros (titulo, autor, genero, año_publicado, isbn, precio, calificacion, stock_count, editor, num_paginas)
+VALUES ('La Odisea Galáctica', 'Orion Starfield', 'Ciencia ficción', 2005, '1234567890126', 19.99, 4.9, 15, 'Prensa Nebulosa', 450);
